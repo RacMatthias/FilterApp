bdbbddebfd455d3667d5e42a301b69855316ac76
Matthias K <kubbixx@web.de>
2019-07-07 11:16:22 +0200

    implemented function to select other images

--- filter_app.py
+++ filter_app.py
@@ -3,14 +3,7 @@
 
 from PIL import Image, ImageTk
 import tkinter as tk
-from tkinter import filedialog
-
-# size of the window
-WIDTH = 1000
-HEIGHT = 600
-
-# index to select filter
-filterIndex = 0
+from tkinter.filedialog import askopenfilename
 
 def increaseFilterIndex():
     global filterIndex
@@ -23,15 +16,35 @@ def decreaseFilterIndex():
         filterIndex -= 1
     print(filterIndex)
 
+def loadStartingImage():
+    img = Image.open("b39.jpg")
+    tkimage = ImageTk.PhotoImage(img)
+
+    return tkimage
+
+def changeImage():
+    filename = askopenfilename()
+    img = Image.open(filename)
+    image1 = ImageTk.PhotoImage(img)
+    pic.configure(image=image1)
+    pic.image = image1
+    print(filename)
+
 def donothing():
-   pass
+    pass
+
+# size of the window
+WIDTH = 1000
+HEIGHT = 600
+
+# index to select filter
+filterIndex = 0
 
 # creates main window
 root = tk.Tk()
 
 # opens image and converts it to tkinter compatible image
-img = Image.open("b39.jpg")
-tkimage = ImageTk.PhotoImage(img)
+image = loadStartingImage()
 
 # create canvas to set the window size
 canvas = tk.Canvas(root,  width=WIDTH, height=HEIGHT)
@@ -47,7 +60,7 @@ filterIndicator = tk.Label(frame, text="Filter", fg="white", bg="black",
 filterIndicator.place(relx=0.4, rely=0, relwidth=0.2, relheight=0.1)
 
 # label that shows the current image
-pic = tk.Label(frame, image=tkimage, height=400, width=600)
+pic = tk.Label(frame, image=image, height=400, width=600)
 pic.place(relx=0.2, rely=0.1, relwidth=0.6, relheight=0.8)
 
 # buttons to switch filters
@@ -57,10 +70,10 @@ buttonLeft.place(relx=0.1, rely=0.1, relwidth=0.1, relheight=0.8)
 buttonRigth = tk.Button(frame, text="right", command=increaseFilterIndex)
 buttonRigth.place(relx=0.8, rely=0.1, relwidth=0.1, relheight=0.8)
 
+# create menuebar
 menubar = tk.Menu(root)
 filemenu = tk.Menu(menubar, tearoff=0)
-filemenu.add_command(label="New", command=donothing)
-filemenu.add_command(label="Open", command=donothing)
+filemenu.add_command(label="Open", command=changeImage)
 filemenu.add_command(label="Save", command=donothing)
 filemenu.add_separator()
 filemenu.add_command(label="Exit", command=root.quit)
@@ -73,4 +86,5 @@ menubar.add_cascade(label="Help", menu=helpmenu)
 
 root.config(menu=menubar)
 
+# keep main window opened
 root.mainloop()
